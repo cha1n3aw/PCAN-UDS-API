666cbfdd54db8e4029c0a3d34f80b5689f63b97e
Ilya <ilyenator2001@gmail.com>
2022-10-11 10:02:08 +0500

    gittest

--- ServiceHandler.cs
+++ ServiceHandler.cs
@@ -81,6 +81,7 @@ namespace PCAN_UDS_TEST
                         strings.Add($"{(char)dataArray[y]}");
                         break;
                     case (0xF1, 0x92):
+
                         y += 2;
                         break;
                 }
